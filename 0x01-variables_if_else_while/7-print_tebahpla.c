3e1eda127b5679309699fce534d32065c96a9b87
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-07-14 16:32:03 -0700

    reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char letter;
+
+	for (char = 'z'; char >= 'a'; z--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
