2080945bca8547169afa4528e63c9b207fc01052
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-07-18 18:12:23 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main-entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = "a"; ch <= "z"; zh++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
