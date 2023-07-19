e5d4285e6f1a46ea8bae7a8e8917a94f027d807e
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-07-18 18:46:32 -0700

    absolute num

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -12,8 +12,8 @@
 int _isalpha(int c)
 {
 	/*use ASCII code to chck conditions*/
-	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
+	if ((c >= 97 && c <= 122) ||
+	    (c >= 65 && c <= 90))
 		return (1);
 	return (0);
 }
