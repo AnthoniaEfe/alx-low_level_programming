201c59417d1eb307c532a1fcc3e06cf636ba7a80
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-07-20 13:44:01 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - checks uppercase
+* Return: 1, upper
+* 0, lower
+*/
+
+int main(void)
+{
+	int c;
+
+	if (isupper(c) > 0)
+		return (1);
+	else
+		return (0);
+}
