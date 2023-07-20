168a1b810b9eb64eb073245cc6a49954b18ee2c3
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-07-20 14:18:39 -0700

    print nums

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * print_numbers - prints numbers 0 to 9
+ *
+ * Return: return 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
+
