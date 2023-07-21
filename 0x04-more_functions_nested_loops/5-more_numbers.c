30ee45b97778fb11fc00447bbd1111b54b3dc2a9
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-07-20 17:41:04 -0700

    corrections

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,23 +2,25 @@
 
 /**
  * more_numbers - print 0 - 14, 10 times
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
 */
 
 void more_numbers(void)
 {
-	int i;
+	int n, i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 1; i <= 10; ++i)
 	{
-		int j;
-
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j <= 14; ++j)
 		{
-			_putchar(j + 48);
+			n = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				n = j % 10;
+			}
+			_putchar(n + 48);
 		}
-
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
