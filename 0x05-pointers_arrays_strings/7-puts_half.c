2291ec9c1e8adaa25dd932b00692f7292adb607d
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-07-24 19:44:23 -0700

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,20 +10,19 @@
 
 void puts_half(char *str)
 {
-	int i, len;
+	int len, n;
 
-	for (len = 0; str[len] != '\0'; ++len)
+	for (len = 0; str[len] != '\0'; ++l)
 		;
 
 	if (len % 2 == 0)
 	{
-		for (i = len / 2; i != '\0'; ++i)
-			_putchar(str[i]);
-	}
-	else
+		for (n = len / 2; str[n] != '\0'; ++n)
+			_putchar(str[n]);
+	} else
 	{
-		for (i = ((len - 1) / 2) + 1; i != '\0'; ++i)
-			_putchar(str[i]);
+		for (n = ((len - 1) / 2) + 1; str[n] != '\0'; ++n)
+			_putchar(str[n]);
 	}
 	_putchar('\n');
 }
