56eb8e26fbb4ec13058349c4e0c476e5d0084d38
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-07-27 16:05:16 -0700

    1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337
+ *
+ * @s: string input
+ *
+ * Return: @s
+*/
+
+char *leet(char *s)
+{
+	int i, c = 0;
+	int lc[] = {97, 101, 111, 116, 108};
+	int uc[] = {65, 69, 79, 84, 76};
+	int n[] = {52, 51, 48, 55, 49};
+
+	while (s[c] != '\0')
+	{
+		/**
+		 * check if value is lowercase or uppercase
+		 * in lc[] and uc[] respectively
+		 * if yes, replace with value n[]
+		*/
+		for (i = 0; i < 5; i++)
+		{
+			if (s[c] == sl[i] || s[c] == ul[i])
+			{
+				s[c] = n[i];
+				break;
+			}
+		}
+		c++;
+	}
+	return (s);
+}
