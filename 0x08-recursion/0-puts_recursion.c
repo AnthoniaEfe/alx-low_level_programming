edfeb19560fc5785a54ab9468eab01a4d4847612
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-08-02 12:41:06 -0700

    in reverse

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion - a function that prints a new line
+ * _puts_recursion - function that prints a string
+ *		followed by a new line
  *
  * @s: the string
  *
@@ -10,10 +11,10 @@
 
 void _puts_recursion(char *s)
 {
-	if (s == '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
+		_puts_recursion(s++);
 	}
 	else
 	{
