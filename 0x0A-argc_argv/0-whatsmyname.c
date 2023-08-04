4a79143a3084e450f0f1b9d8b4603ee59c66362b
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-08-04 16:19:49 -0700

    main.h

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - prints the name of the program
+ * main - prints name of the program
+ *
  * @argc: number of arguments
  * @argv: array of arguments
  *
