bf407177a302bcabb22515bd8ba2eb0a25f307d0
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-08-14 16:43:47 -0700

    betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-#define struct dog
+/**
+ * struct dog - dog structure
+ *
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ */
+
+struct dog
 {
 	char *name;
 	float age;
