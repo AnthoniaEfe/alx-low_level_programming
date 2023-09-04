93eb8a7728531fe231c1ca98ff33a0014e6eb0ef
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-09-04 16:46:03 -0700

    copy file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,6 +22,10 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
+	/**
+	 * 0600, octal value specifying file permissions
+	 * for the newly created file, i.e rw-------
+	 */
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
